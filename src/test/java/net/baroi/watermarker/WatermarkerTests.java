758c5f5e30057855dc5ed04a5cc046d16e172d49
Aldrin Baroi <aldrin.baroi@gmail.com>
2023-02-12 07:56:44 +0000

    Fixed method name typo

--- src/test/java/net/baroi/watermarker/WatermarkerTests.java
+++ src/test/java/net/baroi/watermarker/WatermarkerTests.java
@@ -11,15 +11,15 @@ class WatermarkerTests {
     @Test
     void manualText() {
         try {
-            final String sourceFileName = "resources/pdfClown_userGuide.pdf";
-            final String targetFileName = "resources/pdfClown_userGuide_watermarked.pdf";
+            final String sourceFileName = "src/test/resources/pdfClown_userGuide.pdf";
+            final String targetFileName = "src/test/resources/pdfClown_userGuide_watermarked.pdf";
             final String watermarkText = "My Watermark 1 2023/02/11";
             //
             final FontColor rgbColor = new FontColor(255f, 0f, 0f);
             final Font watermarkFont = new Font(FontType.HELVETICA_BOLD, 50f, rgbColor);
             final File sourcePdfFile = new File(sourceFileName);
             final File targetPdfFile = new File(targetFileName);
-            Watermarker.satermarkPdfFile(watermarkText, watermarkFont, sourcePdfFile, targetPdfFile);
+            Watermarker.watermarkPdfFile(watermarkText, watermarkFont, sourcePdfFile, targetPdfFile);
         } catch (Exception e) {
             fail("Wartermarking failed.", e);
         }
